4efc8b132d3a6d54b8906ab6de6a6772e89f1694
Vaughn Reed <vaughn_reed@outlook.com>
2017-10-27 10:24:18 -0500

    added df with elf ID, ear length class, and gc content

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -14,3 +14,11 @@ get_size_class <- function(ear_length){
    ear_lengths <- ifelse(ear_length > 10, "large", "small")
    return(ear_lengths)
 }
+df <- data.frame(ID=houseelf_earlength_dna_data$id, gc=NA, ear_length_class=NA)
+for(i in 1:nrow(df)){
+  df$gc= elf_dna_gc_content(houseelf_earlength_dna_data$dnaseq)
+}
+for(i in 1:nrow(df)){
+  df$ear_length_class=get_size_class(houseelf_earlength_dna_data$earlength)
+}
+print(df)
