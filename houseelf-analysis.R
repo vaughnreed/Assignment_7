e9370ad37749a1a82eb8edb0c1a5c812ae41883a
Vaughn Reed <vaughn_reed@outlook.com>
2017-10-27 09:43:47 -0500

    added gc_content function

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,2 +1,11 @@
 #I have created this file.  This file will allow me to help with version control.  
 houseelf_earlength_dna_data <- read.csv("./data/houseelf_earlength_dna_data_1.csv", header = TRUE)
+elf_dna_gc_content <- function(sequence){
+  g_elf <- str_count(str_to_upper(sequence), "G")
+  c_elf <- str_count(str_to_upper(sequence), "C")
+  gc_content <- (g_elf + c_elf) / str_length(sequence) * 100
+  return(gc_content)
+}
+library(stringr)
+sapply(houseelf_earlength_dna_data$dnaseq, elf_dna_gc_content)
+
